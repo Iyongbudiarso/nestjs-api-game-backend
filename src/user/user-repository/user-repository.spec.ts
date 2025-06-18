21551fb123c745be1db1d0d14fc52b5ebb2f7fcc
Iyong Budiarso <iyong.budiarso@bukalapak.com>
2025-06-18 10:15:41 +0700

    Add unit tests for UserRepository methods

--- src/user/user-repository/user-repository.spec.ts
+++ src/user/user-repository/user-repository.spec.ts
@@ -5,15 +5,61 @@ import { PrismaService } from "../../prisma/prisma/prisma.service";
 describe("UserRepository", () => {
   let provider: UserRepository;
 
+  const userMock = { id: 1 };
+  let createMock: jest.Mock;
+  let findUniqueMock: jest.Mock;
+
   beforeEach(async () => {
+    createMock = jest.fn().mockResolvedValue(userMock);
+    findUniqueMock = jest.fn().mockResolvedValue(userMock);
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [UserRepository, PrismaService],
-    }).compile();
+    })
+      .overrideProvider(PrismaService)
+      .useValue({
+        user: {
+          create: createMock,
+          findUnique: findUniqueMock,
+        },
+      })
+      .compile();
 
     provider = module.get<UserRepository>(UserRepository);
   });
 
   it("should be defined", () => {
     expect(provider).toBeDefined();
+  });
+
+  it("method save", async () => {
+    const save = await provider.save("username", "password");
+    expect(createMock).toHaveBeenCalledWith({
+      data: {
+        username: "username",
+        password: "password",
+      },
+    });
+    expect(save).toEqual(userMock);
+  });
+
+  it("method findByUsername", async () => {
+    const find = await provider.findByUsername("username");
+    expect(findUniqueMock).toHaveBeenCalledWith({
+      where: {
+        username: "username",
+      },
+    });
+    expect(find).toEqual(userMock);
+  });
+
+  it("method findById", async () => {
+    const find = await provider.findById(1);
+    expect(findUniqueMock).toHaveBeenCalledWith({
+      where: {
+        id: 1,
+      },
+    });
+    expect(find).toEqual(userMock);
   });
 });
